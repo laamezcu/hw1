c105a8e3ad5001a4d734fe104fe8cd7f385a8d9a
Luis Amezcua <laamezcu@usc.edu>
2025-01-30 19:29:02 -0800

    first commit

--- split.cpp
+++ split.cpp
@@ -11,6 +11,7 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +19,19 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if (in == NULL) return;
+  Node* out = in->next;
+  if (in->value % 2 == 1) {
+    odds = in;
+    in->next = NULL;
+    split(out, odds->next, evens);
+  }
+  else {
+    evens = in;
+    in->next = NULL;
+    split(out, odds, evens->next);
+  }
+  in = NULL;
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,36 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+using namespace std;
+
+void printList(Node*& list) {
+  if (list != NULL) {
+    cout << list->value << " ";
+    printList(list->next);
+  }
+}
 
 int main(int argc, char* argv[])
 {
-
+    Node* five = new Node(5, nullptr);
+    Node* four = new Node(4, five);
+    Node* three = new Node(3, four);
+    Node* two = new Node(2, three);
+    Node* one = new Node(1, two);
+    Node* odds = nullptr;
+    Node* evens = nullptr;
+    //Node* in = nullptr;
+    Node* in = one;
+    split(in, odds, evens);
+    cout << "Odds: ";
+    printList(odds);
+    cout << endl;
+    cout << "Evens: ";
+    printList(evens);
+    cout << endl;
+    if (in == NULL) {
+        cout << "in empty" << endl;
+    }
+    return 0;
 }
